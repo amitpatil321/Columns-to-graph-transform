e66e1648744d056e1d708201e8ccfdee423f30e7
Amit <amitpatil321@gmail.com>
2023-10-31 15:19:15 +0530

    Finishing touches and other changes

--- src/component/AntdTableToGraph.js
+++ src/component/AntdTableToGraph.js
@@ -1,60 +1,81 @@
-import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from "prop-types";
+import React, { useState } from "react";
 import { SelectableGroup, createSelectable } from "react-selectable-fast";
+import { Table, Dropdown } from "antd";
+import { useSelector, useDispatch } from "react-redux";
 
-import { Table } from "antd";
+import { setSelectedRows, setTableAction } from "../store/appReducer";
+
+import CONFIG from "../config/config";
 
 import "../App.css";
 
-const PrintData = ({ selectableRef, isSelected, isSelecting, children }) => {
-  return (
-    <div
-      ref={selectableRef}
-      className={isSelecting || isSelected ? "selected" : null}
-    >
-      {children}
-    </div>
-  );
-};
+const items = [
+  {
+    label: "Clustered Chart",
+    key: "clustered",
+  },
+  {
+    label: "Stacked Chart",
+    key: "stacked",
+  },
+  {
+    label: "Pie Chart",
+    key: "pie",
+  },
+  {
+    type: "divider",
+  },
+  {
+    label: "Export to CSV",
+    key: "csv",
+  },
+];
 
-const SelectableComponent = createSelectable(PrintData);
+const AntdTableToGraph = ({ columns, dataSource }) => {
+  const dispatch = useDispatch();
+  const { uniqId } = useSelector(({ appReducer }) => appReducer);
 
-const AntdTableToGraph = (props) => {
-  const { id, rowKey, columns, dataSource, handleSelection } = props;
+  const [colsSelected, setColsSelected] = useState();
 
   const onSelection = (selectedKeys) => {
-    let dataArr = [];
+    const dataArr = [];
+    setColsSelected(selectedKeys);
     selectedKeys.map((each) => {
       const { selectableKey, children } = each.props;
-      const [id, column] = selectableKey.split("-");
-      dataArr.push({ id: id, [column]: children });
+      const [id, column] = selectableKey.split(CONFIG.KEY_SEPERATOR);
+      dataArr.push({ [CONFIG.UNIQID_KEY]: id, [column]: children });
     });
     const result = Object.values(
       dataArr.reduce((grouped, item) => {
-        const { id, ...rest } = item;
-        if (!grouped[id]) {
-          grouped[id] = { id };
+        const { [CONFIG.UNIQID_KEY]: uuid, ...rest } = item;
+        if (!grouped[uuid]) {
+          grouped[uuid] = { uuid };
         }
-        Object.assign(grouped[id], rest);
+        Object.assign(grouped[uuid], rest);
         return grouped;
-      }, {})
+      }, {}),
     );
-    handleSelection(result);
+
+    dispatch(setSelectedRows(result));
   };
 
-  let modColumns = columns.map((column) => {
-    return {
-      ...column,
-      render(text, record, index) {
-        return {
-          children: (
-            <SelectableComponent selectableKey={`${record._id}-${column.key}`}>
-              {text}
-            </SelectableComponent>
-          ),
-        };
-      },
-    };
-  });
+  const modColumns = columns.map((column) => ({
+    ...column,
+    render(text, record, index) {
+      return {
+        children: (
+          <SelectableComponent
+            selectableKey={`${record[[CONFIG.UNIQID_KEY]]}${
+              CONFIG.KEY_SEPERATOR
+            }${column.key}`}>
+            {text}
+          </SelectableComponent>
+        ),
+      };
+    },
+  }));
 
   return (
     <SelectableGroup
@@ -62,10 +83,61 @@ const AntdTableToGraph = (props) => {
       deselectOnEsc
       resetOnStart
       selectOnClick={false}
-    >
-      <Table rowKey={rowKey} columns={modColumns} dataSource={dataSource} />
+      allowClickWithoutSelected>
+      <Dropdown
+        menu={{
+          items,
+          onClick: (selectedOpt) => dispatch(setTableAction(selectedOpt?.key)),
+        }}
+        trigger={["contextMenu"]}
+        disabled={!colsSelected?.length}>
+        <div
+          style={{
+            color: "lightblue",
+            background: "white",
+            height: 200,
+            textAlign: "center",
+            lineHeight: "200px",
+          }}>
+          <Table
+            rowKey={(record) => record[uniqId]}
+            columns={modColumns}
+            dataSource={dataSource}
+            pagination={{
+              pageSize: 15,
+            }}
+          />
+        </div>
+      </Dropdown>
     </SelectableGroup>
   );
 };
+
+AntdTableToGraph.propTypes = {
+  columns: PropTypes.shape({
+    map: PropTypes.func,
+  }),
+  dataSource: PropTypes.oneOfType([PropTypes.object]),
+};
+
+const PrintData = ({ selectableRef, isSelected, isSelecting, children }) => (
+  <div
+    ref={selectableRef}
+    className={isSelecting || isSelected ? "selected" : null}>
+    {children}
+  </div>
+);
+
+PrintData.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  isSelected: PropTypes.bool,
+  isSelecting: PropTypes.bool,
+  selectableRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]),
+};
+
+const SelectableComponent = createSelectable(PrintData);
 
 export default AntdTableToGraph;
